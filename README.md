c5fe5ec83ec408bef7dcfc892474033e7b96c422
Maickel Pirchiner <maickelpc@gmail.com>
2019-02-10 15:41:11 -0200

    Configuração para uso com Mysql

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Para inicializar, é necessário ter instalado o python3 ou superior e seguir os
 1 - Acessar a raiz do projeto
 2 - Ativar o venv com o comando (em linux) ". <diretorio venv>/bin/activate".
 3 - Iniciar o server, de dentro do diretorio <API> com o comando (em linux) "python manage.py runserver"
+
+
+Para uso sem MYSQL comentar o bloco de código de configuração do mysql em api/api/settings.py e descomentar a parte do codigo para sqlite3
--- api/api/settings.py
+++ api/api/settings.py
@@ -76,18 +76,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # Configuração para sqlite3
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'api_django',
-    #     'USER': 'api_django',
-    #     'PASSWORD': 'api_django',
-    #     'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-    #     'PORT': '3306',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+
+    #Configuração para Mysql
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api_django',
+        'USER': 'api_django',
+        'PASSWORD': 'api_django',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
 }
 
 
