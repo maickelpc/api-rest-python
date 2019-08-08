2b8f30e720be4806af9687f6cd9cb9b8397914cf
Maickel <maickelpc@gmail.com>
2019-08-08 19:13:45 -0300

    ...

--- api/core/models.py
+++ api/core/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile',on_delete=models.PROTECT)
     image = models.ImageField(upload_to='image_perfil', null=True, blank=True)
 
     def __str__(self):
         return self.user.first_name;
-
-
-        
